bc5e3ab35c8497d43b33420f78b02810e6901b5e
Your Name <you@example.com>
2019-08-17 21:02:18 +0800

    首页完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,117 @@
 <template>
   <div class="index">
-    首页
+    <el-container>
+      <el-header>
+        <div class="logo">
+          <img src="../assets/logo.png" alt />
+        </div>
+        <div class="title">
+          <h1>电商后台管理系统</h1>
+        </div>
+        <div class="autologin">
+          欢迎光临~
+          <a href="javascript:;" @click='logut'>退出</a>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            unique-opened
+
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+
+              <template v-slot:title>
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item index='1-1'>
+                <i class="el-icon-menu"></i>
+                <span slot="title">用户列表</span>
+              </el-menu-item>
+            </el-submenu>
+
+            <el-submenu index="2">
+              <template v-slot:title>
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+              <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">角色列表</span>
+              </el-menu-item>
+            </el-submenu>
+  </el-menu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logut () {
+      this.$confirm('亲, 你确认要退出系统吗?', '温馨提示', {
+        type: 'warning'
+      }).then(() => {
+        this.$message.success('退出成功')
+        localStorage.removeItem('token')
+        this.$router.push('login')
+      }).catch(() => {
+        this.$message('取消退出')
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+.index {
+  height: 100%;
+  .el-container {
+    height: 100%;
+    .el-header {
+      background-color: #d8d8d8;
+      display: flex;
+      .logo {
+        width: 180px;
+        img {
+          height: 40px;
+          margin: 10px;
+        }
+      }
+      .title {
+        flex: 1;
+        text-align: center;
+        height: 60px;
+        line-height: 60px;
+        color: #545c64;
+      }
+      .autologin {
+        text-align: right;
+        height: 60px;
+        line-height: 60px;
+        font-weight: 700;
 
+        a {
+          color: orange;
+        }
+      }
+    }
+    .el-aside {
+      background: #545c64;
+      .el-menu{
+        border-right: none
+      }
+    }
+    .el-main {
+      background: #ecf0f1;
+    }
+  }
+}
 </style>
