a61b4c132a7e4aceb52fa07762de9b2b8f9a10d6
Your Name <you@example.com>
2019-08-17 11:01:11 +0800

    基本完成

---
+++ src/components/Login.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="login">
+   <el-form ref="form" :model="form" :rules='rules' label-width="80px" status-icon>
+     <img src="../assets/avatar.jpg" alt="" class="logo">
+  <el-form-item label="用户名" prop='username'>
+    <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop='password'>
+    <el-input type='password' v-model="form.password" placeholder="请输入密码"></el-input>
+  </el-form-item>
+
+  <el-form-item>
+    <el-button type="primary" @click="login">登录</el-button>
+    <el-button @click='restForm'>重置</el-button>
+  </el-form-item>
+</el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 12, message: '长度为3到12位', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 12, message: '长度为3到12位', trigger: ['change', 'blur'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    restForm () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate((idvalid) => {
+        if (!idvalid) return
+        console.log(idvalid)
+        // 发送ajax请求
+        axios.post('http://localhost:8888/api/private/v1/login', this.form).then(res => {
+          const meta = res.data.meta
+          if (res.data.meta.status === 200) {
+            this.$message({
+              message: meta.msg,
+              type: 'success'
+            })
+            this.$router.push('/index')
+          } else {
+            console.log(meta.msg)
+            this.$message({
+              message: meta.msg,
+              type: 'error'
+            })
+          }
+        })
+      })
+    }
+  }
+
+}
+
+</script>
+
+<style lang="scss">
+   .login{
+     height: 100%;
+     width: 100%;
+     background-color:#2d434c;
+     overflow: hidden;
+     .el-form{
+       width: 400px;
+       margin: 0 auto;
+       background-color: #fff;
+       padding: 20px;
+       margin-top: 200px;
+       padding-top: 80px;
+       position: relative;
+       .el-button--default{
+         margin-left: 80px
+       }
+       .logo{
+         position: absolute;
+         top: -70px;
+         left: 50%;
+         transform: translateX(-50%);
+         border-radius: 50%;
+         border: 5px solid #fff
+       }
+     }
+   }
+</style>
